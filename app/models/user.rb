ac60e36f4f662537385ff65e1212a41d95864706
Aji Achmad Mustofa <micro280991@gmail.com>
2014-09-14 18:56:26 +0700

    Fix Add Jobs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :security_code, presence: true, on: :create
   validates_format_of :security_code, with: /191014280991/, on: :create
   attr_accessor :security_code
+
+  scope :confirmed, -> { where("confirmed_at IS NOT NULL") }
 end
